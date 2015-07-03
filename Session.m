1771c15f8221fcea2e9af5e866d94bbbe85977e2
fastily <fastily@yahoo.com>
2015-07-02 17:50:15 -0700

    session initialized

--- Session.m
+++ Session.m
@@ -1,3 +1,17 @@
 classdef Session
-    
+    properties
+        sessionID
+        subject
+        dataPath
+        sessionFolder
+        singleUnits
+        trials
+        history
+    end
+    methods
+        function process(session)
+            % loop through the channels, detect spikes, sort and then
+            % output singleUnits
+        end        
+    end
 end
