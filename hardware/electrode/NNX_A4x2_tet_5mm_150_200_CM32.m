285fbbf05b1ce91dbb5e29116749a217d2c5d164
fastily <fastily@yahoo.com>
2015-07-15 14:52:32 -0700

    removeid unnecesaary properties

--- hardware/electrode/NNX_A4x2_tet_5mm_150_200_CM32.m
+++ hardware/electrode/NNX_A4x2_tet_5mm_150_200_CM32.m
@@ -1,9 +1,4 @@
 classdef NNX_A4x2_tet_5mm_150_200_CM32<electrode
-    properties
-        % ## dont need because defined in superclass
-        %impedance = NaN;
-        %disabled = [];
-    end
     
     properties (Constant=true)
         groupings = {[1 2]};
@@ -11,18 +6,19 @@ classdef NNX_A4x2_tet_5mm_150_200_CM32<electrode
     
     methods
         %% constructor
-        function e = NNX_A4x2_tet_5mm_150_200_CM32()
+        function e = NNX_A4x2_tet_5mm_150_200_CM32(varargin)
             e.type = 'Neuronexus';
             % ## changing for now to test - e.numChans = 32;
-            e.numChans = 2;
+            e.numChans = 32;
             e.configuration = '4 Shank; 2 tets per shank';
+            
         end % electrode
         
         %% getPotentialTrodes (returns which channels to group together).
         function trodes = getPotentialTrodes(etrode)
             trodes = [];
             for groups = 1:length(etrode.groupings)
-                trodes = [trodes trode(etrode.groupings{groups})];
+                trodes = [trodes setdiff(trode(etrode.groupings{groups}),etrode.disabled)];
             end
         end
 
