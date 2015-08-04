5421fe6f075100e5a0e0866256a8258fb5619c58
rrecatto <rrecatto@gmail.com>
2015-08-04 16:58:58 -0700

    fixed few issues with spike detection

--- hardware/electrode/NNX_A4x2_tet_5mm_150_200_CM32.m
+++ hardware/electrode/NNX_A4x2_tet_5mm_150_200_CM32.m
@@ -1,7 +1,9 @@
 classdef NNX_A4x2_tet_5mm_150_200_CM32<electrode
     
     properties (Constant=true)
-        groupings = {[1],[2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13],[14], [15], [16], [17], [18],[19], [20], [21], [22], [23], [24], [25], [26], [27], [28], [29], [30], [31], [32]};
+        mapChans = [];
+        groupings = {[1,3,6,8],[2,4,5,7],[9,11,14,16],[10,12,13,15],[17,19,22,24],[18,20,21,23],[25,27,30,32],[26,28,29,31]};
+        %groupings = {[1],[2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13],[14], [15], [16], [17], [18],[19], [20], [21], [22], [23], [24], [25], [26], [27], [28], [29], [30], [31], [32]};
         %groupings = {[1,2],[3,4]}
     end
     
@@ -20,7 +22,8 @@ classdef NNX_A4x2_tet_5mm_150_200_CM32<electrode
             [~,~,~,~,etrode.disabled] = rankContinFiles([path, '\', folder],1); 
             for groups = 1:length(etrode.groupings)
                 if ~isempty(setdiff((etrode.groupings{groups}),etrode.disabled))
-                    trodes = [trodes trode(setdiff((etrode.groupings{groups}),etrode.disabled))];
+%                     trodes = [trodes trode(setdiff((etrode.groupings{groups}),etrode.disabled))];
+                    trodes = [trodes trode(setdiff(etrode.mapChans(etrode.groupings{groups}),etrode.disabled))];
                 end
             end
         end
