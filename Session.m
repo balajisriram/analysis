ee211d56671fc23f27c02fc5cdd48b4385cea29d
balajisriram <balajisriram@gmail.com>
2015-08-05 15:51:12 -0700

    fleshed out the history function

--- Session.m
+++ Session.m
@@ -66,20 +66,57 @@ classdef Session
         function session = detectSpikes(session)
             for i = 1:length(session.trodes)
                 dataPath = fullfile(session.sessionPath,session.sessionFolder);
-                session.trodes(i) = session.trodes(i).detectSpikes(dataPath);
+                try
+                    session.trodes(i) = session.trodes(i).detectSpikes(dataPath);
+                    det.identifier = 'Session.detectSpikes';
+                    det.message = sprintf('detected on trode %d of %d',i, length(session.trodes));
+                    session.addToHistory('Completed',det)
+                catch ex
+                    session.addToHistory('Error',ex)
+                end
             end
         end
 
         function session = sortSpikes(session)
             for i = 1:length(session.trodes)
-                session.trodes(i) = session.trodes(i).sortSpikes();
+                try
+                    session.trodes(i) = session.trodes(i).sortSpikes();
+                    det.identifier = 'Session.sortSpikes';
+                    det.message = sprintf('sorted on trode %d of %d',i, length(session.trodes));
+                    session.addToHistory('Completed',det)
+                catch ex
+                    session.addToHistory('Error',ex)
+                end
             end
         end
-      
         
-        function [fileName] = saveSession(sess)  % save session as a struct to mat file
+        function fileName = saveSession(sess)  % save session as a struct to mat file
             fileName = [sess.sessionFolder,'___',int2str(sess.timeStamp),'.mat'];
             save(fileName,'sess', '-v7.3'); %for some reason wouldnt save correctly unless '-v7.3' command added
+        end
+        
+        %% Manipulating the history
+        function sess = flushHistory(sess)
+            sess.history = {};
+        end
+        
+        function displayHistory(sess)
+            if ~isempty(sess.history)
+                fprintf('#\tTYPE\tIDENT\t\t\t\t\t\tMESSAGE\n')
+            end
+            for i = 1:length(sess.history)
+                fprintf('%d.\t%s\t%s\t\t\t\t\t\t%s\n',i,sess.history{i}{1},sess.history{i}{2},sess.history{i}{3});
+            end
+        end
+        
+        function sess = addToHistory(sess,type,details)
+            switch lower(type)
+                case {'err','error'}
+                    % details is exception object
+                    sess.history{end+1} = {'Err.',details.identifier,details.message,details.stack};
+                case 'completed'
+                    sess.history{end+1} = {'Comp.',details.identifier,details.message};
+            end
         end
     end
 end
