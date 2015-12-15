cae74fcc9db0d1419c9804cf2b3d16c2e67a4be6
rrecatto <rrecatto@gmail.com>
2015-12-14 16:09:58 -0800

    commented and restructured some code for clarity, changed max possible spikes from 50 to 30. Pushing up processAndSaveSession function as well as basic README

---
+++ helpers/processAndSaveSession.m
@@ -0,0 +1,24 @@
+function sess = processAndSaveSession( dataFolder )
+
+% add paths
+addpath(genpath(pwd));
+
+%these remain constant no matter what file is selected
+monitor = ViewSonicV3D245('one');
+etrode = NNX_A1x32_Poly3_5mm_25s_117();
+rigState = StandardRigJuly2015();
+
+
+sessionPath = dataFolder(1:find(dataFolder=='\',1, 'last')-1);
+sessionFolder = dataFolder((find(dataFolder=='\',1, 'last')+1):length(dataFolder));
+subject = sessionFolder(1:find(sessionFolder == '_')-1);
+trialDataPath = fullfile(sessionPath,sessionFolder);
+
+sess = Session(subject,sessionPath,sessionFolder,trialDataPath, etrode, monitor, rigState);
+
+sess = process(sess);
+    
+saveSession(sess);
+
+end
+
