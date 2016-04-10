1bf08b7f977139b3d0872b370e20db4eee76f322
Balaji Sriram <balajisriram@gmail.com>
2016-04-10 13:41:43 -0700

    new changes

--- classes/Session.m
+++ classes/Session.m
@@ -628,6 +628,7 @@ classdef Session
             
         end
         
+
         function sess = zeroNoise(sess)
             for i = 1:length(sess.trodes)
                 sess.trodes(i).spikeWaveForms(sess.trodes(i).spikeAssignedCluster==1,:,:) = 0;
