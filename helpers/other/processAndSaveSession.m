765d52d21f50947620d74e6d464d3a1de377d322
Balaji Sriram <balajisriram@gmail.com>
2016-06-13 06:52:14 -0700

    small cheanges

--- helpers/other/processAndSaveSession.m
+++ helpers/other/processAndSaveSession.m
@@ -8,12 +8,19 @@ monitor = ViewSonicV3D245('one');
 etrode = NNX_A1x32_Poly3_5mm_25s_117();
 rigState = StandardRigJuly2015();
 
-
-sessionPath = dataFolder(1:find(dataFolder=='\',1, 'last')-1);
-sessionFolder = dataFolder((find(dataFolder=='\',1, 'last')+1):length(dataFolder));
-subject = sessionFolder(1:find(sessionFolder == '_')-1);
-trialDataPath = fullfile(sessionPath,sessionFolder);
-
+switch computer
+    case 'MACI64'
+        sessionPath = dataFolder(1:find(dataFolder=='/',1, 'last')-1);
+        sessionFolder = dataFolder((find(dataFolder=='/',1, 'last')+1):length(dataFolder));
+        subject = sessionFolder(1:find(sessionFolder == '_')-1);
+        trialDataPath = fullfile(sessionPath,sessionFolder);
+    otherwise
+        
+        sessionPath = dataFolder(1:find(dataFolder=='\',1, 'last')-1);
+        sessionFolder = dataFolder((find(dataFolder=='\',1, 'last')+1):length(dataFolder));
+        subject = sessionFolder(1:find(sessionFolder == '_')-1);
+        trialDataPath = fullfile(sessionPath,sessionFolder);
+end
 sess = Session(subject,sessionPath,sessionFolder,trialDataPath, etrode, monitor, rigState);
 
 %decides which mappings to use based on MACaddress of computer running
@@ -22,7 +29,7 @@ sess = Session(subject,sessionPath,sessionFolder,trialDataPath, etrode, monitor,
 if strcmp(b, 'F8BC128444CB') || strcmp(b, '6805CA25DFB1') %two analysis computers
     mappings = 'phys';
 else                                %otherwise must be one of behavior computers
-    mappings = 'behavior';
+    mappings = 'phys';
 end
 
 sess = process(sess, mappings);
