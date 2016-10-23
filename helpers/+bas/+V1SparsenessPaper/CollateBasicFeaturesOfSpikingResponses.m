ef353095a297405fc38c4057fa1c444455b4040e
balaji <balajisriram@gmail.com>
2016-10-23 14:50:46 -0700

    some basic scripting

--- helpers/+bas/+V1SparsenessPaper/CollateBasicFeaturesOfSpikingResponses.m
+++ helpers/+bas/+V1SparsenessPaper/CollateBasicFeaturesOfSpikingResponses.m
@@ -35,10 +35,17 @@ end
 f = figure;
 f.Position = [-1566 476 1109 420];
 ax = subplot(1,2,1);
-hist(firingRates,40);
+[count, centers] = hist(firingRates,40);
+b = bar(centers,count);
+b.EdgeColor = 'none';
+b.FaceColor = [0.5, 0.5, 0.5];
 
 ax = subplot(1,2,2); hold on;
-hist(log10(firingRates),40);
+[count,centers] = hist(log10(firingRates),40);
+b = bar(centers,count);
+b.EdgeColor = 'none';
+b.FaceColor = [0.5, 0.5, 0.5];
+
 ax.XTick = [log10(0.01:0.01:0.1) log10(0.2:0.1:1) log10(2:1:10) log10(20:10:100)];
 ax.XTickLabel = {};
 
@@ -48,5 +55,11 @@ Labels{end+1} = '1'; for i = 1:8,Labels{end+1} = '';end
 Labels{end+1} = '10';for i = 1:8,Labels{end+1} = '';end
 Labels{end+1} = '100';
 ax.XTickLabel = Labels;
+%% 
+x = -10:0.01:10;
+y1 = exp(-(x+6).^2/4); y1 = y1/sum(y1);
+y2 = exp(-(x-2).^2/10);y2 = y2/sum(y2);
+plot(x,y1,x,y2,x,y1+y2);
 
-[mu,sig,muci,sigmaci] = normfit()
+
+obj = fitgmdist(log10(firingRates'),2)
