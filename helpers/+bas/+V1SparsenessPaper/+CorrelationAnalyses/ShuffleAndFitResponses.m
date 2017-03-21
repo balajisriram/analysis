faed3d83d751a75b318586c8d93eee600f589003
balaji <balajisriram@gmail.com>
2017-03-21 08:23:36 -0700

    try-catch

--- helpers/+bas/+V1SparsenessPaper/+CorrelationAnalyses/ShuffleAndFitResponses.m
+++ helpers/+bas/+V1SparsenessPaper/+CorrelationAnalyses/ShuffleAndFitResponses.m
@@ -1,6 +1,6 @@
 function FITS = ShuffleAndFitResponses
 if ~exist('SPIKEDETAILS','var')
-    load('DetailsAt500')
+    load('DetailsAt500MS')
 end
 tempSeed = rng;
 rng(736775);
@@ -8,17 +8,23 @@ FIT_LOGISTIC = {};
 
 
 for i = 1:length(SPIKEDETAILS)
-    fitsThisSession = {};
-    separatedResponses = bas.V1SparsenessPaper.CorrelationAnalyses.separateResponsesByStimulus(SPIKEDETAILS{i});
-    numShuffles = 100;
-    for j = 1:numShuffles
-        fitsThisShuffle.shuffledSession = bas.V1SparsenessPaper.CorrelationAnalyses.createSessionFromShuffledResponses(bas.V1SparsenessPaper.CorrelationAnalyses.shuffleResponses(separatedResponses));
-        fitsThisShuffle.LogisticRegression = bas.V1SparsenessPaper.CorrelationAnalyses.FitLogisticRegressionToGivenSession(fitsThisShuffle.shuffledSession);
-        fitsThisSession{end+1} = fitsThisShuffle;
-    end
-    FitDataName = sprintf('FitThisSession%d_Shuffle.mat',i);
+    try
+        fprintf('%d\n',i)
+        fitsThisSession = {};
+        separatedResponses = bas.V1SparsenessPaper.CorrelationAnalyses.separateResponsesByStimulus(SPIKEDETAILS{i});
+        numShuffles = 100;
+        for j = 1:numShuffles
+            fprintf('%d.',j)
+            fitsThisShuffle.shuffledSession = bas.V1SparsenessPaper.CorrelationAnalyses.createSessionFromShuffledResponses(bas.V1SparsenessPaper.CorrelationAnalyses.shuffleResponses(separatedResponses));
+            fitsThisShuffle.LogisticRegression = bas.V1SparsenessPaper.CorrelationAnalyses.FitLogisticRegressionToGivenSession(fitsThisShuffle.shuffledSession);
+            fitsThisSession{end+1} = fitsThisShuffle;
+        end
+        fprintf('\n');
+        FitDataName = sprintf('FitThisSession%d_Shuffle.mat',i);
         save(FitDataName,'fitsThisSession')
-        
+    catch ex
+        getReport(ex)
+    end
     
 end
 end
